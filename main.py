df1fdd19fc4db84b88285398c0f1879f3c565635
yash111206 <yashghuikar6@gmail.com>
2026-01-20 10:28:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-        return render_template('home.html', events=events)
+        return render_template('home.html')
     
 # ---------------- MAIN ----------------
 
 if __name__ == "__main__":
     app.run(host='0.0.0',debug=True)
+
 
 
